3b89ccf3cef30de063b3ee5fd56327d5654d8918
Raghav10026 <raghav10026@gmail.com>
2021-02-04 14:59:56 +0530

    Even Odd

---
+++ Nagarro BootCamp/src/EvenOrOdd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if (n%2==0) {
+			System.out.println("Number is Even");
+		}
+		else {
+			System.out.println("No. is Odd");
+		}
+
+	}
+
+}
