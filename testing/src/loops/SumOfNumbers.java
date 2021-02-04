105b358782e29f34839a04b0076d01cd050d8a8d
Raghav10026 <raghav10026@gmail.com>
2021-02-04 18:10:23 +0530

    Used while loop, for loop, took input from user

--- testing/src/loops/SumOfNumbers.java
+++ testing/src/loops/SumOfNumbers.java
@@ -1,14 +1,27 @@
 package loops;
 
+import java.util.Scanner;
+
 public class SumOfNumbers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int sum=0;
-		int n=9;
-		for (int i=1;i<n;i++)
+//		// TODO Auto-generated method stub
+//		int sum=0;
+//		int n=9;
+//		for (int i=1;i<n;i++)
+//			sum=sum+i;
+//		System.out.println(sum);
+
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+     	int sum=0;
+     	int i=1;
+//		for (int i=1;i<=n;i++)
+//			sum=sum+i;
+//		System.out.println("Sum is: "+sum);
+		while(i<=n) {
 			sum=sum+i;
-		System.out.println(sum);
-	}
+		    i++;
+	}System.out.println(sum);}
 	
 }
