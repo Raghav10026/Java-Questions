c21016fe201725da421e0204ab837a8c01fac6f6
Raghav10026 <raghav10026@gmail.com>
2021-02-06 02:35:56 +0530

    Tenth Pattern

---
+++ Nagarro BootCamp/src/Patterns/PatternTenth.java
@@ -0,0 +1,28 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class PatternTenth {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int nst = 2 * n - 1;
+		int nsp = 0;
+
+		int row = 1;
+		while (row <= n) {
+			for (int csp = 1; csp <= nsp; csp++)
+				System.out.print("  ");
+			for (int cst = 1; cst <= nst; cst++)
+				System.out.print("* ");
+			System.out.println();
+			row = row + 1;
+			nst = nst - 2;
+			nsp = nsp + 1;
+		}
+
+	}
+
+}
