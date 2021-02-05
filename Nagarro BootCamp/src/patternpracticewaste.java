91e40453b0577a354e7b983ba2936d14d13b244b
Raghav10026 <raghav10026@gmail.com>
2021-02-05 18:20:22 +0530

    Pattern 5x5

---
+++ Nagarro BootCamp/src/patternpracticewaste.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class patternpracticewaste {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int row=1;
+		int col=1;
+		while (row<=n) {
+			for (col=1;col<n;col++) {
+				System.out.print("*");
+			}
+			System.out.println("*");
+			row++;
+		}
+	}
+}
