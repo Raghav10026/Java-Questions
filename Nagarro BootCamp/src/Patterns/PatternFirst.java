de85bbd528ac1121cc94d4b3cd389acec20d22bb
Raghav10026 <raghav10026@gmail.com>
2021-02-06 01:17:32 +0530

    Pattern 1st

---
+++ Nagarro BootCamp/src/Patterns/PatternFirst.java
@@ -0,0 +1,24 @@
+package Patterns;
+//Patter is like: *****
+//                *****
+//                *****
+//                *****
+//                *****
+
+public class PatternFirst {
+
+	public static void main(String[] args) {
+		int n=5;
+		int row=1;
+		while (row<=n) {
+			 for(int col=1;col<=n;col++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			row=row+1;
+		}
+		
+
+	}
+
+}
