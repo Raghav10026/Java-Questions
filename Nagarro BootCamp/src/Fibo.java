be9386406937b086b72a828f4b0f3d6bcfd0a0e4
Raghav10026 <raghav10026@gmail.com>
2021-02-05 18:34:58 +0530

    Fibonacci Nth Number/series etc

--- Nagarro BootCamp/src/Fibo.java
+++ Nagarro BootCamp/src/Fibo.java
@@ -1,50 +1,71 @@
+
+import java.util.Scanner;
+
+public class Fibo {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int a = 0;
+		int b = 1;
+		for (int count = 0; count < n; count++) {
+			
+			int sum = a + b;
+			a = b;
+			b = sum;
+
+		}
+		System.out.println(a);
+
+	}
+
+}
 //import java.util.Scanner;
 //
 //public class Fibo {
-//
 //	public static void main(String[] args) {
+//
 //		Scanner sc = new Scanner(System.in);
 //		int n = sc.nextInt();
-//		int a = 0;
-//		int b = 1;
-//		int count = 0;
-//		int sum = 0;
-//		for (count = 0; count < n + 1; count++) {
-//			System.out.println(b);
-//			sum = a + b;
+//		int a = 0; // 0th fibonacci
+//		int b = 1; // 1st fibonacci
+//
+//		int i = 0;
+//		while (i < n) {
+//
+//			int sum = a + b;
 //			a = b;
 //			b = sum;
 //
+//			i++;
 //		}
 //
+//		System.out.println(a);
 //	}
-//
 //}
-import java.util.Scanner;
-class Fibo
-{
-    // Function to find the nth Fibonacci number
-    public static int fib(int n)
-    {
-        if (n <= 1) {
-            return n;
-        }
- 
-        int previousFib = 0, currentFib = 1;
-        for (int i = 0; i < n - 1; i++)
-        {
-            int newFib = previousFib + currentFib;
-            previousFib = currentFib;
-            currentFib = newFib;
-        }
- 
-        return currentFib;
-    }
- 
-    public static void main(String[] args)
-    {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        System.out.println(fib(n));
-    }
-}
+//
+//    // Function to find the nth Fibonacci number
+//    public static int fib(int n)
+//    {
+//        if (n <= 1) {
+//            return n;
+//        }
+// 
+//        int previousFib = 0, currentFib = 1;
+//        for (int i = 0; i < n - 1; i++)
+//        {
+//            int newFib = previousFib + currentFib;
+//            previousFib = currentFib;
+//            currentFib = newFib;
+//        }
+// 
+//        return currentFib;
+//    }
+// 
+//    public static void main(String[] args)
+//    {
+//        Scanner sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+//        System.out.println(fib(n));
+//    }
+//}
