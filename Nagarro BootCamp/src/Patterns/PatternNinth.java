9d1c6dbf4733140ee11305f970fdb18dda1813be
Raghav10026 <raghav10026@gmail.com>
2021-02-06 01:57:50 +0530

    Ninth Pattern

---
+++ Nagarro BootCamp/src/Patterns/PatternNinth.java
@@ -0,0 +1,29 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class PatternNinth {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int nst=1;
+		int nsp=n-1;
+		
+		int row=1;
+		while(row<=n) {
+			for(int csp=1;csp<=nsp;csp++)
+				System.out.print("  ");
+			for(int cst=1;cst<=nst;cst++)
+				System.out.print("* ");
+			System.out.println();
+			row=row+1;
+			nst=nst+2;
+			nsp=nsp-1;
+		}
+
+
+	}
+
+}
