decba3cc10e6baf4c9f67298416edb47460ff941
Raghav10026 <raghav10026@gmail.com>
2021-02-06 01:28:58 +0530

    Pattern third

--- Nagarro BootCamp/src/Patterns/PatternThird.java
+++ Nagarro BootCamp/src/Patterns/PatternThird.java
@@ -5,8 +5,18 @@ import java.util.Scanner;
 public class PatternThird {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int nst = n;
+		int row = 1;
+		while (row <= n) {
+			for (int cst = 1; cst <= nst; cst++)
+				System.out.print("*");
+
+			System.out.println();
+			row = row + 1;
+			nst = nst - 1;
+		}
 
 	}
 
