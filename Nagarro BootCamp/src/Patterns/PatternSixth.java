9a5493d0c472ecf4e0df5b1756e5176f59e1afee
Raghav10026 <raghav10026@gmail.com>
2021-02-06 01:50:14 +0530

    Sixth Pattern

---
+++ Nagarro BootCamp/src/Patterns/PatternSixth.java
@@ -0,0 +1,28 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class PatternSixth {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int nst=n;
+		int nsp=0;
+		
+		int row=1;
+		while(row<=n) {
+			for(int csp=1;csp<=nsp;csp++)
+				System.out.print("  ");
+			for(int cst=1;cst<=nst;cst++)
+				System.out.print("* ");
+			System.out.println();
+			row=row+1;
+			nst=nst-1;
+			nsp=nsp+2;
+		}
+
+
+	}
+
+}
