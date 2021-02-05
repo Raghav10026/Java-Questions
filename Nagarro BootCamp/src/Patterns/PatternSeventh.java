ed73f42b1bc8e41f9421ef707574ee8b9b6c06ad
Raghav10026 <raghav10026@gmail.com>
2021-02-06 01:52:07 +0530

    need to complete/ not done yet

--- Nagarro BootCamp/src/Patterns/PatternSeventh.java
+++ Nagarro BootCamp/src/Patterns/PatternSeventh.java
@@ -1,9 +1,28 @@
 package Patterns;
 
+import java.util.Scanner;
+
 public class PatternSeventh {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int nst=n;
+		int nsp=0;
+		
+		int row=1;
+		while(row<=n) {
+			for(int csp=1;csp<=nsp;csp++)
+				System.out.print("  ");
+			for(int cst=1;cst<=nst;cst++)
+				System.out.print("* ");
+			System.out.println();
+			row=row+1;
+			nst=nst-1;
+			nsp=nsp+1;
+		}
+
 
 	}
 
