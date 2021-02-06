aac14c14cbc5625981a560e06aeced681d94bcb6
Raghav10026 <raghav10026@gmail.com>
2021-02-07 00:01:27 +0530

    Simple Input HackerBlocks Question

---
+++ Nagarro BootCamp/src/HackerBlocks/SimpleInput.java
@@ -0,0 +1,20 @@
+package HackerBlocks;
+
+import java.util.Scanner;
+
+public class SimpleInput {
+	public static void main(String args[]) {
+		Scanner scn = new Scanner(System.in);
+		int sum = 0;
+		
+		while (sum >= 0) {
+
+			int n = scn.nextInt();
+			sum += n;
+
+			if (sum < 0)
+				break;
+			System.out.println(n);
+		}
+	}
+}
