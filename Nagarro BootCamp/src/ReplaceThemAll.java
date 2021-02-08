933090e71c1efb9d4130a45207c5397992802b55
Raghav10026 <raghav10026@gmail.com>
2021-02-08 17:14:26 +0530

    replace 0s by 5

--- Nagarro BootCamp/src/ReplaceThemAll.java
+++ Nagarro BootCamp/src/ReplaceThemAll.java
@@ -5,33 +5,49 @@ public class ReplaceThemAll {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		long n = sc.nextLong();
-		long rem = 0;
-		long rev = 0;
-		long rev2=0;
-		long rem2=0;
-		if(n==0) {
-			n=5;
-		}
-		while(n!=0) {
-			rem=n%10;
-			if(rem==0) {
-				rem=5;
-			}
-			rev=rev*10+rem;
-			n=n/10;
-		}
-		while(rev!=0)
-		 {
-            rem2=rev%10;
-            rev2=rev2*10+rem2;
-            rev=rev/10;
+//		long rem = 0;
+//		long rev = 0;
+//		long rev2=0;
+//		long rem2=0;
+//		if(n==0) {
+//			n=5;
+//		}
+//		while(n!=0) {
+//			rem=n%10;
+//			if(rem==0) {
+//				rem=5;
+//			}
+//			rev=rev*10+rem;
+//			n=n/10;
+//		}
+//		while(rev!=0)
+//		 {
+//            rem2=rev%10;
+//            rev2=rev2*10+rem2;
+//            rev=rev/10;
+//        }
+//        System.out.println(rev2);
+		if(n == 0) {
+            System.out.println(5);
+            return;
         }
-        System.out.println(rev2);
+        long rem = 0, count = 1, ans = 0;
+        while (n> 0) {
+            rem = n % 10;
+            if (rem == 0) {
+                rem = 5;
+            }
+            ans = rem * count + ans;
 
+            count = count * 10;
+            n = n / 10;
+        }
+        System.out.println(ans);
+
+    }
     
 
         }
 	
 		
-}
 
