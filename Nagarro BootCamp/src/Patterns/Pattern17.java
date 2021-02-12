064b82c36301878068674bbd2c11ea1093d8c0e4
Raghav10026 <raghav10026@gmail.com>
2021-02-12 12:13:03 +0530

    17 Pattern

---
+++ Nagarro BootCamp/src/Patterns/Pattern17.java
@@ -0,0 +1,36 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern17 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int nst=n/2;
+		int nsp=2;
+		int row=1;
+		while(row<=n) {
+			for(int cst=1;cst<=nst;cst++)
+				System.out.print("* ");
+			for(int csp=1;csp<=nsp;csp++)
+				System.out.print("  ");
+			for(int cst=1;cst<=nst;cst++)
+				System.out.print("* ");
+			System.out.println();
+			if(row<=n/2)
+				nst--;
+			else
+				nst++;
+			if(row<=n/2)
+				nsp=nsp+2;
+			else
+				nsp=nsp-2;
+			row=row+1;
+				
+			
+		}
+
+	}
+
+}
