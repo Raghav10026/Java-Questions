cee8995f4c8b88dd568c95c0b7c8c4bec19ee204
Raghav10026 <raghav10026@gmail.com>
2021-02-12 12:14:59 +0530

    Seventeenth Pattern

--- Nagarro BootCamp/src/Patterns/Pattern17.java
+++ Nagarro BootCamp/src/Patterns/Pattern17.java
@@ -5,30 +5,29 @@ import java.util.Scanner;
 public class Pattern17 {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		int nst=n/2;
-		int nsp=2;
-		int row=1;
-		while(row<=n) {
-			for(int cst=1;cst<=nst;cst++)
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int nst = n / 2;
+		int nsp = 2;
+		int row = 1;
+		while (row <= n) {
+			for (int cst = 1; cst <= nst; cst++)
 				System.out.print("* ");
-			for(int csp=1;csp<=nsp;csp++)
+			for (int csp = 1; csp <= nsp; csp++)
 				System.out.print("  ");
-			for(int cst=1;cst<=nst;cst++)
+			for (int cst = 1; cst <= nst; cst++)
 				System.out.print("* ");
 			System.out.println();
-			if(row<=n/2)
+			if (row <= n / 2)
 				nst--;
 			else
 				nst++;
-			if(row<=n/2)
-				nsp=nsp+2;
+			if (row <= n / 2)
+				nsp = nsp + 2;
 			else
-				nsp=nsp-2;
-			row=row+1;
-				
-			
+				nsp = nsp - 2;
+			row = row + 1;
+
 		}
 
 	}
