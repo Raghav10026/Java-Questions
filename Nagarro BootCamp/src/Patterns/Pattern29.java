094f91a410f0f8073aa12dda5faabd12e3abf010
Raghav10026 <raghav10026@gmail.com>
2021-02-12 17:11:29 +0530

    Twenty Ninth Pattern

---
+++ Nagarro BootCamp/src/Patterns/Pattern29.java
@@ -0,0 +1,38 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern29 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int row = 1;
+		int val;
+		int nst = 1;
+		int nsp = n - 1;
+		while (row <= n) {
+			val = row;
+
+			for (int csp = 1; csp <= nsp; csp++)
+				System.out.print("\t");
+			for (int cst = 1; cst <= nst; cst++) {
+
+				if (cst == 1 || cst == nst) {
+					System.out.print(val + "\t");
+
+				} else
+					System.out.print(0 + "\t");
+
+			}
+			System.out.println();
+
+			nst = nst + 2;
+			nsp = nsp - 1;
+			row = row + 1;
+
+		}
+
+	}
+
+}
