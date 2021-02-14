181204a1bbbd0ff01561f2a6bf7304a6b6764380
Raghav10026 <raghav10026@gmail.com>
2021-02-14 19:32:24 +0530

    Understood but very difficult {practise again} important

--- Nagarro BootCamp/src/HackerBlocks/PatternDoubleSidedArrow.java
+++ Nagarro BootCamp/src/HackerBlocks/PatternDoubleSidedArrow.java
@@ -4,45 +4,62 @@ import java.util.Scanner;
 
 public class PatternDoubleSidedArrow {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int nst = 1;
-		int nsp = n - 1;
-		int row = 1;
-		int val;
-		while (row <= n) {
-			val = 1;
-			for (int csp = 1; csp <= nsp; csp++) {
-				System.out.print(" ");
-			}
-			for (int cst = 1; cst <= nst; cst++) {
-				System.out.print(val);
-			}
-			int csp = 1;
-			if (row == 1 || row == n) {
-				csp = 2;
-			}
-			for (; csp <= nsp; csp++) {
-				System.out.print(" ");
-			}
-			int cst = 1;
-			if (row == 1 || row == n) {
-				cst = 2;
-			}
-			for (; cst <= nsp; cst++) {
-				System.out.print(" ");
-			}
-			val++;
-			System.out.println();
-			if (row <= n / 2) {
-				nst++;
-				nsp--;
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt(); // number of rows
+		int nsp1 = n - 1; // initializing number of spaces of 1st batch
+		int nsp2 = -1; // initializing number of spaces of 2nd batch
+		int nst = 1; // initializing number of numbers
+
+		for (int row = 1; row <= n; row++) {
+
+			int val; // starting value of numbers
+
+			if (row <= n / 2 + 1) {
+				val = row; // till mid value is equal to row number
 			} else {
-				nst--;
-				nsp++;
+				val = n - row + 1; // after mid values are total rows - current row +1
 			}
 
-			row = row + 1;
+			// work for spaces
+			for (int csp = 1; csp <= nsp1; csp++) {
+				System.out.print("  ");
+			}
+
+			// work for numbers
+			for (int cst = 1; cst <= nst; cst++) {
+				System.out.print(val + " ");
+				val--;
+			}
+
+			// work for spaces
+			for (int csp = 1; csp <= nsp2; csp++) {
+				System.out.print("  ");
+			}
+
+			int cst = 1;
+			val++;
+			if (row == 1 || row == n) {
+				cst = 2;
+
+			}
+
+			// work for numbers
+			for (; cst <= nst; cst++) {
+				System.out.print(val + " ");
+				val++;
+			}
+
+			// preparation for next iteration
+			if (row <= n / 2) {
+				nsp1 -= 2;
+				nst++;
+				nsp2 += 2;
+			} else {
+				nsp1 += 2;
+				nst--;
+				nsp2 -= 2;
+			}
+			System.out.println();
 		}
 	}
 }
