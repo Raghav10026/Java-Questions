6f6471b5fcabf4f72a83fd02c478697d8644783d
Raghav10026 <raghav10026@gmail.com>
2021-02-14 22:40:38 +0530

    Not complete difficult

---
+++ Nagarro BootCamp/src/HackerBlocks/PatternHourGlass.java
@@ -0,0 +1,51 @@
+package HackerBlocks;
+
+import java.util.Scanner;
+
+public class PatternHourGlass {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int nst = n + 1;
+		int nsp = 0;
+		int row = 1;
+		int count = 1;
+		while (row <= 2 * n + 1) {
+			int val;
+			if (row > n + 1) {
+				val = count;
+				count++;
+			} else {
+				val = n - row + 1;
+			}
+			for (int csp = 1; csp <= nsp; csp++) {
+				System.out.print("  ");
+			}
+			for (int cst = 1; cst <= nst; cst++) {
+				if (cst <= n) {
+					System.out.print(val + " ");
+					val--;
+				} else {
+					System.out.print(val + " ");
+					val++;
+				}
+			}
+			for (int cst = 2; cst <= nst; cst++) {
+				System.out.print(val + " ");
+				val++;
+			}
+
+			System.out.println();
+			if (row <= n) {
+				nst = nst - 1;
+				nsp = nsp + 1;
+			} else {
+				nst = nst + 1;
+				nsp = nsp - 1;
+			}
+			row = row + 1;
+
+		}
+	}
+
+}
