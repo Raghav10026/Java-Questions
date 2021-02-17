24409b753c9f5ee51ce02490416ea900d12d6b5f
Raghav10026 <raghav10026@gmail.com>
2021-02-18 02:39:09 +0530

    practise

--- Nagarro BootCamp/src/HackerblockPractise/PrintArmstrong.java
+++ Nagarro BootCamp/src/HackerblockPractise/PrintArmstrong.java
@@ -1,62 +1,49 @@
-package HackerBlocks;
+package HackerblockPractise;
 
 import java.util.Scanner;
 
-public class PrintingArmstrongNumbers {
+public class PrintArmstrong {
+	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		// TODO Auto-generated method stub
-		int lo=sc.nextInt();
-		int hi=sc.nextInt();
-		printArmstrong(lo, hi);
+		int lo = sc.nextInt();
+		int hi = sc.nextInt();
+		Armstrong(lo, hi);
 
 	}
 
-	public static void printArmstrong(int lo, int hi) {
-
-		for (int n = lo; n <= hi; n = n + 1) {
+	public static void Armstrong(int lo, int hi) {
+		for (int n = lo; n <= hi; n++) {
 			boolean res = isArmstrong(n);
-
 			if (res == true)
 				System.out.println(n);
 		}
 	}
 
 	public static boolean isArmstrong(int n) {
-
+		
 		int nod = noOfDigits(n);
+		
 		int on = n;
-
+		
 		int ans = 0;
-
 		while (n != 0) {
-
 			int rem = n % 10;
-
 			ans = ans + (int) Math.pow(rem, nod);
-
 			n = n / 10;
 		}
-
 		if (ans == on)
 			return true;
 		else
 			return false;
-
 	}
 
 	public static int noOfDigits(int n) {
-
 		int nod = 0;
-
 		while (n != 0) {
 			nod = nod + 1;
 			n = n / 10;
 		}
-
 		return nod;
-
 	}
-
 }
