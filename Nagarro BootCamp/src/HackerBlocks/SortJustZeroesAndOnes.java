6229691e634805fd7b40bbb7f93bde34f9b8d820
Raghav10026 <raghav10026@gmail.com>
2021-02-24 18:05:35 +0530

    Sorting zeroes and ones

---
+++ Nagarro BootCamp/src/HackerBlocks/SortJustZeroesAndOnes.java
@@ -0,0 +1,29 @@
+package HackerBlocks;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortJustZeroesAndOnes {
+	static Scanner sc=new Scanner(System.in);
+	public static void main(String[] args) {
+		int[] arr=userInput();
+		sort(arr);
+		display(arr);
+	}
+	public static int[] userInput() {
+		int n=sc.nextInt();
+		int[] arr=new int[n];
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=sc.nextInt();
+		}
+		return arr;
+	}
+	public static void sort(int[] arr) {
+		Arrays.sort(arr);
+	}
+	public static void display(int[] arr) {
+		for(int val:arr)
+			System.out.print(val+" ");
+	}
+
+}
