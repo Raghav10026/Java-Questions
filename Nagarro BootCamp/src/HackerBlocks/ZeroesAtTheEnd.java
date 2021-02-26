b360c9e19881f11e4866154bbcfd18ae64a46ee8
Raghav10026 <raghav10026@gmail.com>
2021-02-26 14:48:32 +0530

    sort it and reverse it!

---
+++ Nagarro BootCamp/src/HackerBlocks/ZeroesAtTheEnd.java
@@ -0,0 +1,46 @@
+package HackerBlocks;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ZeroesAtTheEnd {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int[] arr = userInput();
+		sort(arr);
+		reverse(arr);
+		display(arr);
+
+	}
+
+	public static int[] userInput() {
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < arr.length; i++)
+			arr[i] = sc.nextInt();
+		return arr;
+	}
+
+	public static void sort(int[] arr) {
+		Arrays.sort(arr);
+		}
+
+	public static void reverse(int[] arr) {
+		int low = 0;
+		int high = arr.length - 1;
+		while (low < high) {
+			int temp = arr[low];
+			arr[low] = arr[high];
+			arr[high] = temp;
+			low++;
+			high--;
+		}
+	}
+
+	public static void display(int[] arr) {
+		for (int val : arr)
+			System.out.print(val + " ");
+	}
+
+}
