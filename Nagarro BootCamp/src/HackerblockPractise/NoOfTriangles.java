62b3c9b075b8a59b470f8f4222b6674996557496
Raghav10026 <raghav10026@gmail.com>
2021-03-01 04:10:05 +0530

    number of triangles from given no in array

--- Nagarro BootCamp/src/HackerblockPractise/NoOfTriangles.java
+++ Nagarro BootCamp/src/HackerblockPractise/NoOfTriangles.java
@@ -1,37 +1,59 @@
 package HackerblockPractise;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class NoOfTriangles {
-static Scanner sc=new Scanner(System.in);
+	static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		int[] arr=userInput();
+		int[] arr = userInput();
 		System.out.println(totalTriangles(arr));
 
 	}
+
 	public static int[] userInput() {
-		int n=sc.nextInt();
-		int [] arr=new int[n];
-		for(int i=0;i<arr.length;i++) {
-			arr[i]=sc.nextInt();
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
 		}
 		return arr;
 	}
-	
+
+//	public static int totalTriangles(int[] arr) {
+//		int count=0;
+//		for(int i=0;i<arr.length;i++) {
+//			for(int j=i+1;j<arr.length;j++) {
+//				for(int k=j+1;k<arr.length;k++) {
+//					if(arr[i]+arr[j]>arr[k] && arr[j]+arr[k]>arr[i] && arr[k]+arr[i]>arr[j]) {
+//						count=count+1;
+//						System.out.println(arr[i]+" "+arr[j]+" "+arr[k]);
+//						
+//					}
+//				}
+//			}
+//		}
+//		return count;
+//	}
 	public static int totalTriangles(int[] arr) {
-		int count=0;
-		for(int i=0;i<arr.length;i++) {
-			for(int j=i+1;j<arr.length;j++) {
-				for(int k=j+1;k<arr.length;k++) {
-					if(arr[i]+arr[j]>arr[k] && arr[j]+arr[k]>arr[i] && arr[k]+arr[i]>arr[j]) {
-						count=count+1;
-						System.out.println(arr[i]+" "+arr[j]+" "+arr[k]);
-						
-					}
+		int count = 0;
+		Arrays.sort(arr);
+		int last = arr.length - 1; // fix the last one because it is highest.
+		for (; last >= 0; last--) { //better complexity
+			int l = 0;
+			int r = last - 1;
+			while (l < r) {
+				if (arr[l] + arr[r] > arr[last]) {
+					count += r - l;
+					r--;
+				} else {
+					l++;
+
 				}
 			}
+
 		}
 		return count;
 	}
-
 }
