c041c94b00b0e13e8cd1e15349f6cc38d52ffcaa
Raghav10026 <raghav10026@gmail.com>
2021-03-08 17:53:03 +0530

    Good Questions

--- Nagarro BootCamp/src/HackerBlocksArrays2dStrings/RainWaterTrapping.java
+++ Nagarro BootCamp/src/HackerBlocksArrays2dStrings/RainWaterTrapping.java
@@ -3,39 +3,53 @@ package HackerBlocksArrays2dStrings;
 import java.util.Scanner;
 
 public class RainWaterTrapping {
-	static Scanner sc=new Scanner(System.in);
+	static Scanner ob = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		int t=sc.nextInt();
-		while(t>0) {
-			int n=sc.nextInt();
-			int[] arr=new int[n];
-			for(int i=0;i<arr.length;i++) {
-				arr[i]=sc.nextInt();
+
+		int t = ob.nextInt();
+
+		while (t-- != 0) {
+
+			int no = ob.nextInt();
+			int[] arr = takeInput(no);
+
+			int sum = 0;
+
+			for (int i = 1; i < arr.length - 1; i++) {
+
+				int lmax = maxArray(0, i, arr);
+				int rmax = maxArray(i, arr.length - 1, arr);
+
+				int min = Math.min(lmax, rmax);
+				sum += min - arr[i];
+
 			}
-			System.out.println(rainWater(arr));
-			t--;
+
+			System.out.println(sum);
+
 		}
+
 	}
-	public static int rainWater(int[] arr) {
-		int one=0;
-		int last=arr.length-1;
-		int sum=0;
-		int trapped=0;
-		for(int i=0;i<arr.length;i++) {
-			for(int j=0;j<arr.length;j++) {
-				if(arr[i]==arr[j]) {
-					//trapped=sum;
-					//j++;
-				}
-				else if(arr[i]>arr[j]) {
-					sum+=arr[i]-arr[j];
-					
-				}
-			}
-			
+
+	public static int[] takeInput(int no) {
+		int[] arr = new int[no];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = ob.nextInt();
 		}
-		return sum;
+
+		return arr;
 	}
-	
+
+	public static int maxArray(int start, int end, int[] arr) {
+		int max = arr[start];
+
+		for (int i = start; i <= end; i++) {
+			if (arr[i] > max)
+				max = arr[i];
+		}
+
+		return max;
+	}
 
 }
