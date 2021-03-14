105ee4eda8c97c505466ec17610a761c4ef59c8b
Raghav10026 <raghav10026@gmail.com>
2021-03-15 03:38:54 +0530

    Merged two Sorted arrays

---
+++ Nagarro BootCamp/src/HackerblockPractise/Merging2SortedArrays.java
@@ -0,0 +1,58 @@
+package HackerblockPractise;
+
+import java.util.Scanner;
+
+public class Merging2SortedArrays {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int n=sc.nextInt();
+		int m=sc.nextInt();
+		int[] arr1 = new int[n];
+		takeInput(arr1);
+		int n1 = arr1.length;
+		int[] arr2 = new int[m];
+		takeInput(arr2);
+		int n2 = arr2.length;
+
+		int[] arr3 = new int[n1 + n2];
+
+		merge(arr1, arr2, n1, n2, arr3);
+
+		System.out.println("Array after merging");
+		for (int i = 0; i < n1 + n2; i++)
+			System.out.print(arr3[i] + " ");
+	}
+
+	public static void merge(int[] arr1, int[] arr2, int n1, int n2, int[] arr3) {
+		int i = 0, j = 0, k = 0;
+
+		// Traverse both array
+		while (i < n1 && j < n2) {
+			// Check if current element of first
+			// array is smaller than current element
+			// of second array. If yes, store first
+			// array element and increment first array
+			// index. Otherwise do same with second array
+			if (arr1[i] < arr2[j])
+				arr3[k++] = arr1[i++];
+			else
+				arr3[k++] = arr2[j++];
+		}
+
+		// Store remaining elements of first array
+		while (i < n1)
+			arr3[k++] = arr1[i++];
+
+		// Store remaining elements of second array
+		while (j < n2)
+			arr3[k++] = arr2[j++];
+	}
+
+	public static void takeInput(int[] arr) {
+		for (int val=0;val<arr.length;val++) {
+			arr[val]=sc.nextInt();
+		}
+	}
+
+}
