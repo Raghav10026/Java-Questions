dcf1138d46bd370c6fa38d3356c7b3af6ede9ca4
Raghav10026 <raghav10026@gmail.com>
2021-03-15 04:09:47 +0530

    maximum occuring character in string

--- Nagarro BootCamp/src/HackerBlocksArrays2dStrings/MaxFrequencyCharacter.java
+++ Nagarro BootCamp/src/HackerBlocksArrays2dStrings/MaxFrequencyCharacter.java
@@ -11,23 +11,38 @@ public class MaxFrequencyCharacter {
 		System.out.println(maxFrequency(str));
 
 	}
-
 	public static char maxFrequency(String str) {
-		int count=0;
-		char[] charray = new char[26]; // To store freq of each character
-		for (int i = 0; i < str.length(); i++) {
-			charray[str.charAt(i) - 'a']++; // Updating freq each time
-		}
-		int max = 0;
-		char ch = 'a';
-		
-		for (int i = 0; i < 26; i++) {
-			if (max <= charray[i]) { // Choosing max Freq Character
-				max = charray[i];
-				ch = (char) (i + 'a');
-			}
-		}
-		System.out.println(max);
-		return ch; // return character
+	int[] arr=new int[127];
+	char ch=' ';
+	for(int i=0;i<str.length();i++) {
+		arr[str.charAt(i)]=arr[str.charAt(i)]+1;
 	}
+	int max=-1;
+	for(int i=0;i<str.length();i++) {
+		if(max<=arr[str.charAt(i)]) {
+			max=arr[str.charAt(i)];
+			ch=str.charAt(i);
+		}
+
+//	public static char maxFrequency(String str) {
+//		int count=0;
+//		char[] charray = new char[26]; // To store freq of each character
+//		for (int i = 0; i < str.length(); i++) {
+//			charray[str.charAt(i) - 'a']++; // Updating freq each time
+//		}
+//		int max = 0;
+//		char ch = 'a';
+//		
+//		for (int i = 0; i < 26; i++) {
+//			if (max <= charray[i]) { // Choosing max Freq Character
+//				max = charray[i];
+//				ch = (char) (i + 'a');
+//			}
+//		}
+//		System.out.println(max);
+//		return ch; // return character
+	}
+	System.out.println(max);
+	return ch;
+}
 }
