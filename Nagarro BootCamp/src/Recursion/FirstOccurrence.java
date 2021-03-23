9c73f7a0eb4fe37f0c7ed5603962ebc75639daf3
Raghav10026 <raghav10026@gmail.com>
2021-03-23 19:48:52 +0530

    first occurrence index in array

---
+++ Nagarro BootCamp/src/Recursion/FirstOccurrence.java
@@ -0,0 +1,25 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class FirstOccurrence {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int[] arr= {40,5,6,2,4,7};
+		int item=4;
+		System.out.println(firstOccur(arr,4,0));
+		
+	}
+	
+	public static int firstOccur(int[] arr, int item,int vidx) {
+		if(vidx>=arr.length) {
+			return -1;
+		}
+		if(arr[vidx]==item) {
+			return vidx;
+		}
+		return firstOccur(arr,item,vidx+1);
+		
+	}
+
+}
