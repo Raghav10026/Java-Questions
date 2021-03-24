17d269ffd30f06851b84b335f30c5e995ae5bada
Raghav10026 <raghav10026@gmail.com>
2021-03-24 21:34:38 +0530

    Subsquence in string, calling 3 times

---
+++ Nagarro BootCamp/src/Recursion/SubsequenceASCII.java
@@ -0,0 +1,28 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class SubsequenceASCII {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		subsASCII(str,"");
+
+	}
+	
+	public static void subsASCII(String ques, String ans) {
+		if(ques.length()==0) {
+			System.out.println(ans);
+			return;
+		}
+		char ch=ques.charAt(0);
+		String roq=ques.substring(1);
+		
+		subsASCII(roq,ans);
+		subsASCII(roq,ans+ch);
+		subsASCII(roq,ans+(int) ch);
+	}
+
+}
