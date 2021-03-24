3608c32f76a276bc054dd89c2a8894602fceed41
Raghav10026 <raghav10026@gmail.com>
2021-03-24 22:56:37 +0530

    recursion practice

--- Nagarro BootCamp/src/RecursionPractice/fibopractice.java
+++ Nagarro BootCamp/src/RecursionPractice/fibopractice.java
@@ -7,19 +7,18 @@ public class fibopractice {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
-		int a=0;
-		int b=1;
-		System.out.println(fibo(a,b,n));
-		
+		System.out.println(fibo(n));
 
 	}
 	
-	public static int fibo(int a, int b, int n) {
-		if()
-			return;
-		int sp=fibo(a,b,n-1);
-		return sp*n;
-			
+	public static int fibo(int n) {
+		if(n==0 || n==1) {
+			return n;
+		}
+		int fnm1=fibo(n-1);
+		int fnm2=fibo(n-2);
+		int fn=fnm1+fnm2;
+		return fn;
 	}
 
 }
