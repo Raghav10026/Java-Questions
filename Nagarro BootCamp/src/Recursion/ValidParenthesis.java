9f85d69fe729e94cf83de73516e1f30d02f95c8f
Raghav10026 <raghav10026@gmail.com>
2021-03-25 21:58:26 +0530

    valid parenthesis question

--- Nagarro BootCamp/src/Recursion/ValidParenthesis.java
+++ Nagarro BootCamp/src/Recursion/ValidParenthesis.java
@@ -14,13 +14,19 @@ public class ValidParenthesis {
 	}
 
 	public static void validParenthesis(int n, int open, int close, String ans) {
-		if (n == open && n == close) {
+
+		// +ve base case, getting answer because of this base case
+		if (n == open && n == close) { // always should be equal to n, both open and closing backet
 			System.out.println(ans);
 			return;
 		}
+		// -ve base case, codinton where we are not getting desired ouptut like
+		// when open brackets are more than n, or closing brackets are more than opening
+		// brackets
 		if (open > n || close > open)
 			return;
-		validParenthesis(n, open + 1, close, ans + "(");
+		validParenthesis(n, open + 1, close, ans + "("); // increasing 1 in open because printing we are printing 1
+															// opening bracket
 		validParenthesis(n, open, close + 1, ans + ")");
 
 	}
