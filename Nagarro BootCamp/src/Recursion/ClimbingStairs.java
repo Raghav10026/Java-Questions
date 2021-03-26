265fedaaddad4683326009b72e37a31432f2e144
Raghav10026 <raghav10026@gmail.com>
2021-03-26 20:46:44 +0530

    climbing stairs! denomination 1,2,3 and total is 5 type questions

---
+++ Nagarro BootCamp/src/Recursion/ClimbingStairs.java
@@ -0,0 +1,32 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class ClimbingStairs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		CS(0,n,"");
+		
+
+	}
+	
+	public static void CS(int curr,int n, String ans) { //3optins can climb 1 stair at a time and 2 stair atm and 3 stair atm
+		if (curr == n) {
+			System.out.println(ans);
+			return;
+		}
+
+		if (curr > n) {
+			return;
+		}
+
+		for (int step = 1; step <= 5; step++)
+			CS(curr + step, n, ans + step);
+
+		}
+	}
+
+
