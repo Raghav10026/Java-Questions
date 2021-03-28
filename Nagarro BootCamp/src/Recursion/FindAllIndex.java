0be07ae33cd18e288ebc26ea99a117900d0a7deb
Raghav10026 <raghav10026@gmail.com>
2021-03-28 22:50:53 +0530

    finding all the index of item in array

--- Nagarro BootCamp/src/Recursion/FindAllIndex.java
+++ Nagarro BootCamp/src/Recursion/FindAllIndex.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class FindAllIndex {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		int[] arr=new int[n];
-		for(int i=0;i<arr.length;i++) {
-			arr[i]=sc.nextInt();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
 		}
 		int[] res = allIndex(arr, 30, 0, 0);
 		for (int val : res)
 			System.out.println(val);
-		
+
 	}
 
-	public static int[] allIndex(int[] arr, int item, int vidx, int count ) {
+	public static int[] allIndex(int[] arr, int item, int vidx, int count) {
 		if (vidx == arr.length) {
 
 			int[] br = new int[count];
@@ -37,6 +37,5 @@ public class FindAllIndex {
 
 		return rr;
 
-		
 	}
 }
