cf4860966ecd6d18dc86cb4b835832edd07d8f6c
Raghav10026 <raghav10026@gmail.com>
2021-03-31 20:25:20 +0530

    coding ninjas

---
+++ testing/src/loops/error.java
@@ -0,0 +1,26 @@
+package loops;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class error {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int p = scn.nextInt();
+		int n = 0;
+		for (int count = 0; count < p; count++) {
+			int ele = 6 * n + 2;
+			if (ele % 4 == 0) {
+				continue;
+			} else {
+				System.out.println(ele);
+				n++;
+			}
+			n++;
+
+		}
+
+	}
+
+}
